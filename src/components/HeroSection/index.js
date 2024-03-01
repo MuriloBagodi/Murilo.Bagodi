0cede552f6e66c35f2c4b73cf8eb8af38422f707
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-03-01 14:36:42 -0300

    fix hero bg

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -41,11 +41,14 @@ const HeroBg = styled.div`
 `;
 
 const HeroInnerContainer = styled.div`
+  position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  @media screen and (max-width: 960px) {
+  max-width: 1100px;
+
+  @media (max-width: 960px) {
     flex-direction: column;
   }
 `;
@@ -53,19 +56,13 @@ const HeroInnerContainer = styled.div`
 const HeroLeftContainer = styled.div`
   width: 100%;
   order: 1;
-  @media screen and (max-width: 960px) {
+  @media (max-width: 960px) {
     order: 2;
-    margin-bottom: 32px;
+    margin-bottom: 30px;
     display: flex;
-    align-items: center;
+    gap: 6px;
     flex-direction: column;
-  }
-  @media screen and (max-width: 640px) {
-    order: 2;
-    margin-bottom: 32px;
-    display: flex;
     align-items: center;
-    flex-direction: column;
   }
 `;
 
@@ -74,31 +71,31 @@ const HeroRightContainer = styled.div`
   order: 2;
   display: flex;
   justify-content: end;
-  gap: 12px;
-
-  @media screen and (max-width: 960px) {
+  @media (max-width: 960px) {
     order: 1;
-    margin-bottom: 80px;
-    justify-content: center;
+    display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-contents: center;
+    margin-bottom: 80px;
   }
-  @media screen and (max-width: 640px) {
-    order: 1;
+
+  @media (max-width: 640px) {
     margin-bottom: 30px;
   }
 `;
 
 const Title = styled.div`
-  font-size: 50px;
   font-weight: 700;
+  font-size: 50px;
   color: ${({ theme }) => theme.text_primary};
   line-height: 68px;
 
-  @media screen and (max-width: 960px) {
+  @media (max-width: 960px) {
     text-align: center;
   }
 
-  @media screen and (max-width: 640px) {
+  @media (max-width: 960px) {
     font-size: 40px;
     line-height: 48px;
     margin-bottom: 8px;
@@ -106,23 +103,21 @@ const Title = styled.div`
 `;
 
 const TextLoop = styled.div`
-  font-size: 32px;
   font-weight: 600;
-  color: ${({ theme }) => theme.text_secondary};
-  line-height: 68px;
-  gap: 12px;
+  font-size: 32px;
   display: flex;
+  gap: 12px;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
 
-  @media screen and (max-width: 960px) {
+  @media (max-width: 960px) {
     text-align: center;
   }
 
-  @media screen and (max-width: 640px) {
+  @media (max-width: 960px) {
     font-size: 22px;
     line-height: 48px;
     margin-bottom: 16px;
-    flex-direction: column;
-    width: 46vh;
   }
 `;
 
@@ -133,17 +128,17 @@ const Span = styled.span`
 
 const SubTitle = styled.div`
   font-size: 20px;
-  color: ${({ theme }) => theme.text_primary + 95};
   line-height: 32px;
-  margin-bottom: 32px;
+  margin-bottom: 42px;
+  color: ${({ theme }) => theme.text_primary + 95};
 
-  @media screen and (max-width: 960px) {
+  @media (max-width: 960px) {
     text-align: center;
   }
 
-  @media screen and (max-width: 640px) {
+  @media (max-width: 960px) {
     font-size: 16px;
-    line-height: 24px;
+    line-height: 32px;
   }
 `;
 
@@ -152,50 +147,56 @@ const ResumeButton = styled.a`
   -moz-appearance: button;
   appearance: button;
   text-decoration: none;
+
   width: 95%;
   max-width: 300px;
   text-align: center;
   padding: 16px 0;
-  color: ${({ theme }) => theme.white};
-  border-radius: 20px;
-  cursor: pointer;
-  font-size: 20px;
-  font-weight: 600;
-  transition: all 0.2s ease-in-out !important;
+
+  background: hsla(271, 100%, 50%, 1);
   background: linear-gradient(
     225deg,
     hsla(271, 100%, 50%, 1) 0%,
     hsla(294, 100%, 50%, 1) 100%
   );
+  background: -moz-linear-gradient(
+    225deg,
+    hsla(271, 100%, 50%, 1) 0%,
+    hsla(294, 100%, 50%, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    225deg,
+    hsla(271, 100%, 50%, 1) 0%,
+    hsla(294, 100%, 50%, 1) 100%
+  );
   box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
+  border-radius: 50px;
+  font-weight: 600;
+  font-size: 20px;
+
   &:hover {
     transform: scale(1.05);
     transition: all 0.4s ease-in-out;
-    box-shadow: rgb(133, 76, 230) 0px 0px 20px;
+    box-shadow: 20px 20px 60px #1f2634;
+    filter: brightness(1);
   }
 
   @media (max-width: 640px) {
     padding: 12px 0;
     font-size: 18px;
   }
+  color: white;
 `;
 
 const Image = styled.img`
-  width: 100%;
-  position: relative;
   border-radius: 50%;
+  width: 100%;
+  height: 100%;
   max-width: 400px;
   max-height: 400px;
-  object-fit: cover;
-  object-position: center;
-  border: ${({ theme }) => theme.primary} 3px solid;
+  border: 2px solid ${({ theme }) => theme.primary};
 
-  @media (max-width: 768px) {
-    max-width: 400px;
-    max-height: 400px;
-  }
-
-  @media screen and (max-width: 640px) {
+  @media (max-width: 640px) {
     max-width: 280px;
     max-height: 280px;
   }
